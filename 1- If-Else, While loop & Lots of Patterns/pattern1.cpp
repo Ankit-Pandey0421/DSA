3f192609b9cbb2446eaae33dad028822561bc9d1
Ankit Pandey <apandey1_be22@thapar.edu>
2025-11-24 00:32:54 +0530

    Updated pattern1.cpp with square pattern logic enhancements

---
+++ 1- If-Else, While loop & Lots of Patterns/pattern1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std ;
+
+void pattern1()
+{
+    // Square Pattern
+    cout << endl;
+    cout << "You are going to see a square pattern:- " << endl;
+
+    int side, i, j;
+    cout << endl;
+    cout << "Enter side of the square:";
+    cout << endl;
+    cin >> side;
+    cout << endl;
+
+    for (i = 1; i <= side; i++)
+    {
+        for (j = 1; j <= side; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
