919ccaf9631ccb0528ae8184497baffa16decaaf
Ankit Pandey <apandey1_be22@thapar.edu>
2025-11-24 01:20:05 +0530

    Add/Update pattern3 and pattern4: implement pattern logic

--- 1- If-Else, While loop & Lots of Patterns/pattern3.cpp
+++ 1- If-Else, While loop & Lots of Patterns/pattern3.cpp
@@ -5,10 +5,10 @@ int main(){
     cin>>n;
     int i = 1;
     while(i<=n){
-        int j = n ;
-        while(j >= 1){
+        int j = 1;
+        while(j <= n){
             cout << j << "  ";
-            j--;
+            j++ ; 
         }
         cout << endl;
         i++;
